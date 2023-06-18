db21ab4f13bf474b23ecc124cd7d38f42b1ae27e
Alan Khismatullin <alanhis043@gmail.com>
2023-06-18 17:38:57 +0300

    Начало переработки интейфейса

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,28 @@ function App() {
 
   useEffect(() => {
     if (value != undefined) {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${
-          value.data.geo_lat
-        }&lon=${value.data.geo_lon}&appid=${
-          import.meta.env.VITE_OPENWEATHER_API
-        }&units=metric&lang=ru`
-      )
-        .then((data) => data.json())
-        .then((result) => {
-          setWeather(result);
-        });
-      fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${
-          value.data.geo_lat
-        }&lon=${value.data.geo_lon}&cnt=5&appid=${
-          import.meta.env.VITE_OPENWEATHER_API
-        }&units=metric&lang=ru`
-      )
-        .then((data) => data.json())
-        .then((result) => {
-          setListWeather(result);
-        });
     }
   }, [value]);
   return (
     <>
-      <div>
-        <AddressSuggestions
-          token={import.meta.env.VITE_DADATA_API}
-          value={value}
-          onChange={setValue}
-          filterFromBound="city"
-          filterToBound="city"
-        />
+      <section id="main-container">
+        <section>
+          <p>Погода</p>
+          <AddressSuggestions
+            token={import.meta.env.VITE_DADATA_API}
+            value={value}
+            onChange={setValue}
+            filterFromBound="city"
+            filterToBound="city"
+          />
+        </section>
+        {/* 
         {weather != undefined && listWeather != undefined ? (
           <WeatherContainer data={weather} list={listWeather.list} />
         ) : (
           <div>Выберите город</div>
-        )}
-      </div>
+        )} */}
+      </section>
     </>
   );
 }
